aafee7c8ee9bb2a569ffa816f5704aa97144b31c
leejay <leejay@ponylink.co.kr>
2025-04-22 11:34:05 +0900

    init project

---
+++ src/main/java/com/workshop/socket_workshop/SocketWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.socket_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshop/socket_workshop/SocketWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.socket_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
